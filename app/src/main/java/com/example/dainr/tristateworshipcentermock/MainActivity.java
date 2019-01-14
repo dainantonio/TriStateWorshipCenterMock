9e3d94c64af16726d35fbf67e79284c2a24e75a4
dainr <dain.russell@gmail.com>
2019-01-14 00:45:10 -0500

    detail added to activity_about file

--- app/src/main/java/com/example/dainr/tristateworshipcentermock/MainActivity.java
+++ app/src/main/java/com/example/dainr/tristateworshipcentermock/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.dainr.tristateworshipcentermock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -24,6 +23,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar replacing ActionBar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
